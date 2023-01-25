f961dae73c027c0d03a20567a35f35429d8ad2e6
Hiroko Matsuyama <110796421+hmatsuyama3@users.noreply.github.com>
2023-01-24 20:31:48 -0500

    Initial commit
    Also added a comment in main!

--- MusicHealsMinds/Java/src/main/java/com/musicHealsMinds/MusicHealsMinds/MusicHealsMindsApplication.java
+++ MusicHealsMinds/Java/src/main/java/com/musicHealsMinds/MusicHealsMinds/MusicHealsMindsApplication.java
@@ -3,6 +3,7 @@ package com.musicHealsMinds.MusicHealsMinds;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Java rocks! :) 
 @SpringBootApplication
 public class MusicHealsMindsApplication {
 
